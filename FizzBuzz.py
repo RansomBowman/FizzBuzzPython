f8e3d11a97aefcf48f3cee504a8aead671c24880
Ransom Bowman <ransombowman@ymail.com>
2021-03-21 19:38:43 -0600

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,17 @@
 def fizzBuzz():
     num = int(input("Enter the number to fizzbuzz: "))
     for i in range(1, num + 1):
-        if not(i%3 == 0) and not(i%5 == 0):
+        if not(i%3 == 0) and not(i%5 == 0) and not (i%7 == 0) and not (i%11 == 0):
             print(i)
         else:
             if (i%3 == 0):
-                print("fizz", end = '')
+                print("Fizz", end = '')
             if (i%5 == 0):
-                print("buzz", end = '')
+                print("Buzz", end = '')
+            if (i%7 == 0):
+                print("Ping", end = '')
+            if (i%11 == 0):
+                print("Pong", end = '')
             print()    
 
 fizzBuzz()
